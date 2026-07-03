63ab839ae17999fc4d008d915ed30f28556ea75e
Cedric Swalens <wp.cedric94@gmail.com>
2026-07-03 20:55:00 +0200

    postgres toegevoegd aan apphost

--- EduFlix.AppHost/AppHost.cs
+++ EduFlix.AppHost/AppHost.cs
@@ -1,5 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.EduFlix_Web>("web");
+// PostgreSQL draait als container. WithDataVolume houdt de data bij tussen restarts,
+// en pgAdmin geeft een web-ui om de database te bekijken tijdens het ontwikkelen.
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+var database = postgres.AddDatabase("eduflixdb");
+
+builder.AddProject<Projects.EduFlix_Web>("web")
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
