403a7bc74d9e77f43947623af22289c2237d278f
Cedric Swalens <wp.cedric94@gmail.com>
2026-07-09 15:16:00 +0200

    account paginas toegevoegd

---
+++ EduFlix.Web/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace EduFlix.Web.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
--- EduFlix.Web/Program.cs
+++ EduFlix.Web/Program.cs
@@ -1,4 +1,8 @@
 using EduFlix.Web.Components;
+using EduFlix.Web.Components.Account;
+using EduFlix.Web.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +14,33 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddMudServices();
+
+// Identity: cascading auth state + de account-services.
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+// Database-context via Aspire (Npgsql). "eduflixdb" komt overeen met de resource in de AppHost.
+builder.AddNpgsqlDbContext<ApplicationDbContext>("eduflixdb");
+
+builder.Services.AddIdentityCore<ApplicationUser>(options =>
+    {
+        // geen e-mailbevestiging nodig voor dit project
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
@@ -28,6 +59,9 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+// Endpoints voor de Identity /Account Razor-componenten.
+app.MapAdditionalIdentityEndpoints();
 
 app.MapDefaultEndpoints();
 
